619965815cb5dcdfe5b7ccdefbe7197a62aa658b
qweri0p <71005650+qweri0p@users.noreply.github.com>
2021-02-10 12:21:32 +0100

    MONKE

--- MONKBOT.py
+++ MONKBOT.py
@@ -1,15 +1,27 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
+client = commands.Bot(command_prefix= '.')
 
 @client.event
 async def on_ready():
     print(f'{client.user} MONKE MONKE MONKE MONKE')
+
+@client.command(aliases=["monke", "MONKE"])
+async def MONKESpam(ctx):
+    await ctx.send("MONKE")
+
+@client.command()
+async def give(ctx):
+    await ctx.send("I give u :banana:.")
+
+@client.command(aliases=["github", "qweriop"])
+async def qweri0p(ctx):
+    await ctx.send("https://github.com/qweri0p/MONKE-BOT")
 
 client.run(TOKEN)
